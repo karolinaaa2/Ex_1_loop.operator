70c9e6663613a54f8d280fdbd4c0bb6c6b53d34d
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:47:03 +0300

    Add project files.

---
+++ Ex_1_loop.operator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Парні числа від 1 до 20:");
+        for (int i = 1; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
